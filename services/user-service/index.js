fa8d3424ba19001afc41920d9c5b7c90d038ee59
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-20 23:11:14 +0530

    Updated connection

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -2,16 +2,31 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-const app = express();
 dotenv.config();
+
+const app = express();
 const mongoDB_URL = process.env.MONGODB_URL;
+const PORT=process.env.PORT||3000;
 
-mongoose.connect(mongoDB_URL, []);
-const connection = mongoose.connection;
+if(!mongoDB_URL){
+    console.error("Error: MONGODB_URL is not defined in environment variables.");
+    process.exit(1);
+}
 
-connection.once("open", () => {
-  console.log("MongoDB connection established successfully");
-});
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+async function startServer(){
+    try{
+        await mongoose.connect(mongoDB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connection established successfully");
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port ${PORT}`);
+        })
+    }
+    catch(error){
+        console.log("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+startServer();
