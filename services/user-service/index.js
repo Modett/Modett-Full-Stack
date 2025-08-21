253661486c73e04a4be4a43c9e0bf316aba1797d
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-21 12:20:23 +0530

    Updated index.js file

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import router from "./src/routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json()); //Middleware to parse JSON request bodies
+
 const mongoDB_URL = process.env.MONGODB_URL;
 const PORT=process.env.PORT||3000;
 
@@ -17,6 +20,7 @@ async function startServer(){
     try{
         await mongoose.connect(mongoDB_URL,[]);
         console.log("MongoDB connection established successfully");
+        app.use("/api/auth",router);
         app.listen(PORT,()=>{
             console.log(`Server is running on port ${PORT}`);
         })
