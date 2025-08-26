b08b0383f8dc34085ae32310ba49b28e82dc8d73
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-26 16:34:21 +0530

    Created orderRouter and simple updates

---
+++ services/user-service/src/routes/order.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { auth } from "../../middleware/auth.middleware.js";
+import { createOrder } from "../controllers/order.controller.js";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/", auth, createOrder);
+
+export default orderRouter;
