482333f9cfc5f190ea50fbc45a15e1fee26ec2bc
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-27 11:57:39 +0530

    created updatePaymentStatus

--- services/user-service/src/controllers/order.controller.js
+++ services/user-service/src/controllers/order.controller.js
@@ -80,3 +80,22 @@ export const updateShippingAddress = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+//admin
+export const updatePaymentStatus=async(req,res)=>{
+  try{
+    const {paymentStatus}=req.body;
+    const order=await Order.findByIdAndUpdate(
+      {_id:req.params.id},
+      {paymentStatus},
+      {new:true}
+    )
+    if(!order){
+      return res.status(404).json({message:"Order not found"});
+    }
+    res.json(order);
+  }
+  catch(error){
+    res.status(500).json({message:"Server error"});
+  }
+}
