57c1d05899803e1f2601f4b319b3a79316040d3c
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-27 19:12:43 +0530

    create updateUserProfile controller function

--- services/user-service/src/controllers/profile.controller.js
+++ services/user-service/src/controllers/profile.controller.js
@@ -106,3 +106,16 @@ export const getProfileById = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+// admin
+export const updateUserProfile = async (req, res) => {
+  const userId = req.params.id;
+  const updateData = req.body;
+  const updatedUser = await User.findByIdAndUpdate(userId, updateData, {
+    new: true,
+  });
+  if (!updatedUser) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json(updatedUser);
+};
