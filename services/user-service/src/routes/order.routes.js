d20f19edbcbe64ee3fe9ffe33c602d9158e4dcc1
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-27 15:21:01 +0530

    updated order.route.js

--- services/user-service/src/routes/order.routes.js
+++ services/user-service/src/routes/order.routes.js
@@ -1,20 +1,34 @@
 import express from "express";
 import { auth } from "../../middleware/auth.middleware.js";
 import isAdmin from "../../middleware/admin.middleware.js";
-import { createOrder,getOrders,getOrderById, cancelOrder,updateShippingAddress,updatePaymentStatus,updateOrderStatus,getAllOrders } from "../controllers/order.controller.js";
+import {
+  createOrder,
+  getOrders,
+  getOrderById,
+  cancelOrder,
+  updateShippingAddress,
+  updatePaymentStatus,
+  updateOrderStatus,
+  getAllOrders,
+} from "../controllers/order.controller.js";
 
 const orderRouter = express.Router();
 
 // user routes
 orderRouter.post("/", auth, createOrder);
 orderRouter.get("/", auth, getOrders);
-orderRouter.get("/:id",auth,getOrderById);
-orderRouter.put("/:id/cancel",auth,cancelOrder);
-orderRouter.put("/:id/shipping-address",auth,updateShippingAddress);
+orderRouter.get("/:id", auth, getOrderById);
+orderRouter.put("/:id/cancel", auth, cancelOrder);
+orderRouter.put("/:id/shipping-address", auth, updateShippingAddress);
 
 // admin routes
-orderRouter.get("/admin/all",auth,isAdmin,getAllOrders);
-orderRouter.put("/admin/:id/payment-status",auth,isAdmin,updatePaymentStatus);
-orderRouter.put("/admin/:id/order-status",auth,isAdmin,updateOrderStatus);
+orderRouter.get("/admin/all", auth, isAdmin, getAllOrders);
+orderRouter.put(
+  "/admin/:id/payment-status",
+  auth,
+  isAdmin,
+  updatePaymentStatus
+);
+orderRouter.put("/admin/:id/order-status", auth, isAdmin, updateOrderStatus);
 
 export default orderRouter;
