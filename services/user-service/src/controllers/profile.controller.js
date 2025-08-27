c3afc0d35363525e0085ddc7a88cbe6610929722
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-27 19:00:20 +0530

    create getPublicProfie controller function

--- services/user-service/src/controllers/profile.controller.js
+++ services/user-service/src/controllers/profile.controller.js
@@ -63,3 +63,20 @@ export const updateBio = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const getPublicProfile = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId).select("name bio");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({
+      name: user.name,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
