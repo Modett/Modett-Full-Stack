43a6028b59da77cd2c4803570325f17a48bece85
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-28 17:50:32 +0530

    created deleteUser controller function

--- services/user-service/src/controllers/auth.controller.js
+++ services/user-service/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import Order from "../models/Order.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
@@ -181,5 +182,21 @@ export const changePassword = async (req, res) => {
     res.json({ message: "Password changed successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    await Order.updateMany({ user: userId }, { $set: { user: null } });
+    await User.findByIdAndDelete(userId);
+
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
 };
