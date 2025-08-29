d0efffdd6fdb72bb048e0b5496d3a2a723c52a4b
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-29 14:49:46 +0530

    updated auth.route.js

--- services/user-service/src/routes/auth.routes.js
+++ services/user-service/src/routes/auth.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { body } from "express-validator";
 import {
   registerUser,
   loginUser,
@@ -22,32 +21,21 @@ import isAdmin from "../../middleware/admin.middleware.js";
 
 const authRouter = express.Router();
 
-const registerValidationRules = [
-  body("name").notEmpty().withMessage("Name is required"),
-  body("email").isEmail().withMessage("Invalid email"),
-  body("phone").notEmpty().withMessage("Phone is required"),
-  body("password")
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters"),
-];
-
-const loginValidationRules = [
-  body("emailOrPhone").notEmpty().withMessage("Email or phone is required"),
-  body("password").notEmpty().withMessage("Password is required"),
-];
-
-authRouter.post("/register", registerUser);
-authRouter.post("/login", loginUser);
+// User routes
 authRouter.get("/protected", auth, (req, res) => {
   res.json({ message: "Protected route accessed", user: req.user });
 });
 authRouter.get("/", auth, getProfile);
-authRouter.put("/", auth, updateProfile);
+authRouter.post("/register", registerUser);
+authRouter.post("/login", loginUser);
 authRouter.post("/forgot-password", forgotPassword);
 authRouter.post("/reset-password", resetPassword);
 authRouter.post("/change-password", auth, changePassword);
-authRouter.delete("/", auth, deleteUser);
+authRouter.put("/", auth, updateProfile);
 authRouter.put("/change-email-or-phone", auth, changeEmailOrPhone);
+authRouter.delete("/", auth, deleteUser);
+
+// Admin routes
 authRouter.get("/admin/get-all-users", auth, isAdmin, getAllUsers);
 authRouter.get("/admin/get-user/:id", auth, isAdmin, getUserById);
 authRouter.put("/admin/update-user-role/:id", auth, isAdmin, updateUserRole);
