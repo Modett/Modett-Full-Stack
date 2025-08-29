53fa957d006a03be12dab59bf458de8f4a834b78
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-29 13:44:08 +0530

    created getUserByID controller function

--- services/user-service/src/controllers/auth.controller.js
+++ services/user-service/src/controllers/auth.controller.js
@@ -248,3 +248,18 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+// admin
+
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
