22485a40a78aa2692fb3ec5d4a8db8c266bc7947
Tasheen2002 <darshikatasheen99@gmail.com>
2025-09-01 15:19:32 +0530

    created measurement routes for user role

--- services/user-service/src/routes/measurement.routes.js
+++ services/user-service/src/routes/measurement.routes.js
@@ -1,4 +1,11 @@
-import { createMeasurement} from "../controllers/measurement.controller.js";
+import {
+  createMeasurement,
+  updateMeasurement,
+  getCurrentMeasurement,
+  deleteMeasurement,
+  getAllUserMeasurements,
+  getMeasurementById,
+} from "../controllers/measurement.controller.js";
 import express from "express";
 import { auth } from "../../middleware/auth.middleware.js";
 import isAdmin from "../../middleware/admin.middleware.js";
@@ -6,5 +13,35 @@ import { measurementValidationRules } from "../utils/measurement.validator.js";
 
 const measurementRouter = express.Router();
 
-measurementRouter.post("/", auth, measurementValidationRules, createMeasurement);
+measurementRouter.post(
+  "/",
+  auth,
+  measurementValidationRules,
+  createMeasurement
+);
+measurementRouter.put("/", auth, measurementValidationRules, updateMeasurement);
+measurementRouter.get(
+  "/",
+  auth,
+  measurementValidationRules,
+  getCurrentMeasurement
+);
+measurementRouter.delete(
+  "/",
+  auth,
+  measurementValidationRules,
+  deleteMeasurement
+);
+measurementRouter.get(
+  "/all",
+  auth,
+  measurementValidationRules,
+  getAllUserMeasurements
+);
+measurementRouter.get(
+  "/:id",
+  auth,
+  measurementValidationRules,
+  getMeasurementById
+);
 export default measurementRouter;
