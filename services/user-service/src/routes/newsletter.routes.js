4ef4b971f7b55c508c6acb6dd8b1be4ce4b384b0
Tasheen2002 <darshikatasheen99@gmail.com>
2025-09-03 09:30:22 +0530

    Added validation rules for controller function added route path for controller function

--- services/user-service/src/routes/newsletter.routes.js
+++ services/user-service/src/routes/newsletter.routes.js
@@ -1,7 +1,16 @@
 import express from "express";
+import { auth } from "../../middleware/auth.middleware.js";
+import isAdmin from "../../middleware/admin.middleware.js";
+import { newsletterValidationRules } from "../utils/newsletter.validator.js";
+import { subscribeToNewsletter } from "../controllers/newsletter.controller.js";
+
 const newsLetterRouter = express.Router();
 
-
-
+newsLetterRouter.post(
+  "/subscribe",
+  auth,
+  newsletterValidationRules,
+  subscribeToNewsletter
+);
 
 export default newsLetterRouter;
